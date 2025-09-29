b3eeee84e4483f519e80f773d851d3ed13e1ea1d
KaratePikachu <KaidenKeyZ@gmail.com>
2025-09-29 11:08:08 -0400

    Eighth note support

--- level/music/beat_sync/beat_sync.gd
+++ level/music/beat_sync/beat_sync.gd
@@ -1,31 +1,26 @@
 extends Node
 
-signal offbeat
-signal beat
-
-enum HitTiming{
-	PERFECT,
-	GOOD,
-	BAD
-}
+signal sixteenth_beat
+signal eighth_beat
+signal quarter_beat
 
 
 var start_time : int
 var time_delay : int
 var current_level : LevelData
-
-
-var current_half_crotchet : int:
-	set(new_val):
-		current_half_crotchet = new_val
-		if current_half_crotchet % 2 == 0:
-			beat.emit()
-		else:
-			offbeat.emit()
-		
 var current_time : float
 
-var hit_err : Array[float]
+var current_quarter_crotchets : int:
+	set(new_val):
+		current_quarter_crotchets = new_val
+		if current_quarter_crotchets % 4 == 0:
+			quarter_beat.emit()
+		elif current_quarter_crotchets % 2 == 0:
+			eighth_beat.emit()
+		else:
+			sixteenth_beat.emit()
+		
+
 
 func _ready() -> void:
 	pass
@@ -39,11 +34,11 @@ func play(level : LevelData):
 func _process(delta: float) -> void:
 	if current_level == null:
 		return;
-		
+	
 	_update_time()
-	var half_crotchets : int = int(seconds_to_half_crotchet(current_time))
-	if half_crotchets != current_half_crotchet:
-		current_half_crotchet = half_crotchets
+	var quarter_crotchets : int = int(seconds_to_quarter_crotchets(current_time))
+	if quarter_crotchets != current_quarter_crotchets:
+		current_quarter_crotchets = quarter_crotchets
 
 
 func _update_time() -> void:
@@ -52,11 +47,11 @@ func _update_time() -> void:
 	current_time = max(0, time)
 	
 	
-func seconds_to_half_crotchet(seconds:float) -> float:
+func seconds_to_quarter_crotchets(seconds:float) -> float:
 	if current_level == null:
 		printerr("No level selected")
 	
 	#var remaining_seconds := seconds # I don't think this is necessary in gdscript, but just to be safe
 	#var total_crotchets := 1.0
 	#total_crotchets += remaining_seconds*current_bpm/60 + 1
-	return seconds*current_level.bpm/30.0
+	return seconds*current_level.bpm/15.0
