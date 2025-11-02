a94f37bc55e85850f6fe237c2584db5807d81f6a
Deku2424 <dekusworld@gmail.com>
2025-11-02 13:35:43 -0500

    initial bullet patterns and bullet tweaks
    adds the initial bullet patterns, fixes a bug with the curved bullets, and adds a lifetime to bullets based on sixteenth notes

--- entities/bullets/bullet.gd
+++ entities/bullets/bullet.gd
@@ -10,11 +10,14 @@ var direction : Vector2
 @export var curving : float
 @export var wiggle_freq : float
 @export var wiggle_dist : float
+@export var lifetime : int
+var age : int = 0
 
 func _ready() -> void:
 	assert(color_sprites.size() == 3, "Missing sprites")
 	BeatSync.quarter_beat.connect(_quarter_beat)
 	BeatSync.eighth_beat.connect(_eighth_beat)
+	BeatSync.sixteenth_beat.connect(_sixteenth_beat)
 
 func set_color(color : DamageComponent.DamageColor) -> void:
 	damage_component.color = color
@@ -31,4 +34,10 @@ func _quarter_beat():
 	pass
 
 func _eighth_beat():
+	pass
+
+func _sixteenth_beat():
+	age += 1
+	if (age >= lifetime):
+		queue_free()
 	pass
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -19,24 +19,29 @@ var boost_time : float = -1;
 func _ready() -> void:
 	instance = self
 	BeatSync.quarter_beat.connect(_quarter_beat)
+	$ticker.play("60_bpm")
 
 func _quarter_beat() -> void:
-	$Sprite2D.scale = Vector2(0.6,0.6)
-	boost_time = 0
-	await get_tree().create_timer(0.1).timeout
-	$Sprite2D.scale = Vector2(0.5,0.5)
+	#$Sprite2D.scale = Vector2(0.6,0.6)
+	#boost_time = 0
+	#await get_tree().create_timer(0.1).timeout
+	#$Sprite2D.scale = Vector2(0.5,0.5)
+	pass
 
 
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right","move_up", "move_down")
 	var total_speed : float = speed
-	if boost_time >= 0:
-		total_speed += boost_curve.sample(boost_time)
-		boost_time += delta
-		if boost_time >= boost_curve.max_domain:
-			boost_time = -1
+	
+	#if boost_time >= 0:
+		#total_speed += boost_curve.sample(boost_time)
+		#boost_time += delta
+		#if boost_time >= boost_curve.max_domain:
+			#boost_time = -1
 		
 	velocity = direction*total_speed
+	if (Input.is_action_pressed("focus")):
+		velocity *= 0.5
 	#print(total_speed)
 	move_and_slide()
