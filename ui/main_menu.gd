692244a1172b1e2f9abca6431cdac56eaeade9fe
SinKer-Court <nomoloskefyalew@gmail.com>
2025-11-03 09:57:03 -0500

    MainMenu
    MainMenu

---
+++ ui/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://test_scene.tscn")
+
+
+func _on_options_pressed() -> void:
+	print("Coming Soon...")
+
+
+func _on_exit_game_pressed() -> void:
+	get_tree().exit()
